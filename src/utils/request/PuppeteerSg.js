e3bca0c97d0aa299622a94ca684a841e937f6cd2
tinkismeeee <tinhisme1@gmail.com>
2026-04-16 06:55:58 +0700

    Stop lazyLoad when scroll reaches the end of page

--- src/utils/request/PuppeteerSg.js
+++ src/utils/request/PuppeteerSg.js
@@ -78,36 +78,43 @@ class PuppeteerSg {
 	async injectHelperFunctions(page) {
 		const browserHelpers = `
       window.__helpers__ = {
-        lazyLoad: async (selector = null, rendertime = 100) => {
-          await new Promise(resolve => {
-            const container = selector ? document.querySelector(selector) : null;
-            if (selector && !container) {
-              return resolve();
-            }
-            let prevScroll = 0;
-            const timer = setInterval(() => {
-              if (container) {
-                container.scrollTop += container.clientHeight;
-                if (container.scrollTop === prevScroll) {
-                  clearInterval(timer);
-                  resolve();
-                }
-                prevScroll = container.scrollTop;
-                if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
-                  clearInterval(timer);
-                  resolve();
-                }
-              } else {
-                const scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, window.innerHeight * 0.8);
-                if (window.innerHeight + window.scrollY >= scrollHeight) {
-                  clearInterval(timer);
-                  resolve();
-                }
-              }
-            }, rendertime);
-          });
-        },
+        lazyLoad: async (selector = null, rendertime = 300) => {
+  await new Promise(resolve => {
+    const container = selector ? document.querySelector(selector) : null;
+    if (selector && !container) {
+      return resolve();
+    }
+
+    let prevScroll = -1;
+
+    const timer = setInterval(() => {
+      if (container) {
+        container.scrollTop += container.clientHeight;
+
+        if (container.scrollTop === prevScroll) {
+          clearInterval(timer);
+          resolve();
+          return;
+        }
+
+        prevScroll = container.scrollTop;
+      } else {
+        window.scrollBy(0, window.innerHeight * 0.8);
+
+        const currentScroll = window.scrollY;
+        const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+
+        if (currentScroll === prevScroll || currentScroll >= maxScroll) {
+          clearInterval(timer);
+          resolve();
+          return;
+        }
+
+        prevScroll = currentScroll;
+      }
+    }, rendertime);
+  });
+},
         hideSelectorAll: (selector) => {
           document.querySelectorAll(selector).forEach(el => el.style.display = 'none');
         },
