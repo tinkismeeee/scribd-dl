98a3b3086c72cdc73e9f2964cdfa2772797d23f8
tinkismeeee <tinhisme1@gmail.com>
2026-04-16 06:53:30 +0700

    Add protocolTimeout and change page.goto waitUntil from load to domcontentloaded

--- src/utils/request/PuppeteerSg.js
+++ src/utils/request/PuppeteerSg.js
@@ -1,81 +1,82 @@
-import path from 'path'
-import puppeteer from 'puppeteer'
+import path from "path";
+import puppeteer from "puppeteer";
 import { directoryIo } from "../io/DirectoryIo.js";
 
 class PuppeteerSg {
-  static buffer = 1000;
+	static buffer = 1000;
 
-  constructor() {
-    if (!PuppeteerSg.instance) {
-      PuppeteerSg.instance = this;
-      process.on('exit', () => {
-        this.close();
-      });
-    }
-    return PuppeteerSg.instance;
-  }
+	constructor() {
+		if (!PuppeteerSg.instance) {
+			PuppeteerSg.instance = this;
+			process.on("exit", () => {
+				this.close();
+			});
+		}
+		return PuppeteerSg.instance;
+	}
 
-  /**
-   * Launch a browser
-   */
-  async launch() {
-    const isCI = process.env.CI === 'true'; // Detect if running in CI
-    const args = [];
-    if (isCI) {
-      args.push('--no-sandbox', '--disable-setuid-sandbox');
-    }
-    this.browser = await puppeteer.launch({
-      headless: "new",
-      defaultViewport: null,
-      args,
-      timeout: 0,
-    });
-  }
+	/**
+	 * Launch a browser
+	 */
+	async launch() {
+		const isCI = process.env.CI === "true"; // Detect if running in CI
+		const args = [];
+		if (isCI) {
+			args.push("--no-sandbox", "--disable-setuid-sandbox");
+		}
+		this.browser = await puppeteer.launch({
+			headless: "new",
+			defaultViewport: null,
+			args,
+			timeout: 0,
+			protocolTimeout: 1200000, // it works with 1200 pages
+		});
+	}
 
-  /**
-   * New a page
-   */
-  async getPage(url) {
-    if (!this.browser) {
-      await this.launch()
-    }
-    let page = await this.browser.newPage()
-    await page.goto(url, {
-      waitUntil: "load",
-    })
-    await this.injectHelperFunctions(page)
-    await new Promise(resolve => setTimeout(resolve, this.buffer))
-    return page
-  }
+	/**
+	 * New a page
+	 */
+	async getPage(url) {
+		if (!this.browser) {
+			await this.launch();
+		}
+		let page = await this.browser.newPage();
+		await page.goto(url, {
+			waitUntil: "domcontentloaded",
+		});
+		await this.injectHelperFunctions(page);
+		await new Promise((resolve) => setTimeout(resolve, this.buffer));
+		return page;
+	}
 
-  /**
-   * Generate PDF from the page
-   */
-  async generatePDF(page, pdfPath, options = {}) {
-    await directoryIo.create(path.dirname(pdfPath));
-    await page.pdf({
-      path: pdfPath,
-      printBackground: true,
-      timeout: 0,
-      ...options
-    });
-  }
+	/**
+	 * Generate PDF from the page
+	 */
+	async generatePDF(page, pdfPath, options = {}) {
+		await directoryIo.create(path.dirname(pdfPath));
+		await page.pdf({
+			path: pdfPath,
+			printBackground: true,
+			timeout: 0,
+			...options,
+		});
+	}
 
-  /**
-   * Close the browser
-   */
-  async close() {
-    if (this.browser) {
-      await this.browser.close();
-      this.browser = null;
-    }
-  }
+	/**
+	 * Close the browser
+	 */
+	async close() {
+		if (this.browser) {
+			await this.browser.close();
+			this.browser = null;
+		}
+	}
 
-  /**
-   * Inject helper functions into the page context
-   */
-  async injectHelperFunctions(page) {
-    const browserHelpers = `
+	/**
+	 * Inject helper functions into the page context
+	 */
+	async injectHelperFunctions(page) {
+		const browserHelpers = `
       window.__helpers__ = {
         lazyLoad: async (selector = null, rendertime = 100) => {
           await new Promise(resolve => {
@@ -122,8 +123,8 @@ class PuppeteerSg {
         timeout: (ms) => new Promise(resolve => setTimeout(resolve, ms)),
       };
     `;
-    await page.evaluate(browserHelpers);
-  }
+		await page.evaluate(browserHelpers);
+	}
 }
 
-export const puppeteerSg = new PuppeteerSg()
+export const puppeteerSg = new PuppeteerSg();
